44ccbb96c453cd93699ec3cfe0ccc43dedfe37aa
Dr-Wyatt <daniel.ryuichi.wyatt@gmail.com>
2019-04-01 18:21:26 -0500

    Fixed Javascript tag link issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,52 @@
-var counter = 0;
+function randomNumFunc(){
+    var randomNum = Math.floor((Math.random()*120) + 19);
+    console.log("randomNum", randomNum);
+    $("#Random-Number").text(randomNum);
+}
+var testNum = randomNumFunc();
+var wins = 0;
+var losses = 0;
+var AmethystNum = Math.floor((Math.random()*12) + 1);
+console.log("AmethystNum", AmethystNum);
+var EmeraldNum = Math.floor((Math.random()*12) + 1);
+console.log("EmeraldNum", EmeraldNum);
+var RubyNum = Math.floor((Math.random()*12) + 1);
+console.log("RubyNum", RubyNum);
+var SapphireNum = Math.floor((Math.random()*12) + 1);
+console.log("SapphireNum", SapphireNum);
+var result = 0;
+console.log("result", result);
 
-$(".gems").on("click", function() {
-    counter += 1;
-    
-    alert("Clicked!");
-    // $("#result").append(counter);
-});
+
+
+if (result === testNum){
+    wins++;
+    $("#wins").text(wins);
+    randomNumFunc();
+}
+else if (result > testNum){
+    losses--;
+    $("#losses").text(losses);
+    randomNumFunc();
+}
+else { 
+    $("#Amethyst").on("click", function() {
+        result = parseInt(result) + parseInt(AmethystNum);
+        $("#result").text("Your Score: " + result);
+    });
+
+    $("#Emerald").on("click", function() {
+        result = parseInt(result) + parseInt(EmeraldNum);
+        $("#result").text("Your Score: " + result);
+    });
+
+    $("#Ruby").on("click", function() {
+        result = parseInt(result) + parseInt(RubyNum);
+        $("#result").text("Your Score: " + result);
+    });
+
+    $("#Sapphire").on("click", function() {
+        result = parseInt(result) + parseInt(SapphireNum);
+        $("#result").text("Your Score: " + result);
+    });
+}
